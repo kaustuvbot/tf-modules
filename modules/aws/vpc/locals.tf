27877cb95cb83d4d54f26ed6113c2dbdbae52279
kaustuvprajapati <kaustuvprajapati@gmail.com>
2023-05-20 15:00:00 +0100

    refactor: add VPC input validations and sensible defaults
    Add validation blocks for vpc_cidr (format + prefix range), environment
    (allowed values), project name (length), and AZ count (1-3). Compute
    subnet counts and NAT gateway count in locals for cleaner references.

--- modules/aws/vpc/locals.tf
+++ modules/aws/vpc/locals.tf
@@ -9,5 +9,10 @@ locals {
     var.tags,
   )
 
-  az_count = length(var.availability_zones)
+  az_count             = length(var.availability_zones)
+  public_subnet_count  = length(var.public_subnet_cidrs)
+  private_subnet_count = length(var.private_subnet_cidrs)
+
+  # NAT gateway count: 0 if disabled, 1 if single, or per-AZ
+  nat_gateway_count = var.enable_nat_gateway ? (var.single_nat_gateway ? 1 : local.az_count) : 0
 }
--- modules/aws/vpc/variables.tf
+++ modules/aws/vpc/variables.tf
@@ -2,21 +2,46 @@ variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
   default     = "10.0.0.0/16"
+
+  validation {
+    condition     = can(cidrhost(var.vpc_cidr, 0))
+    error_message = "The vpc_cidr must be a valid CIDR block (e.g., 10.0.0.0/16)."
+  }
+
+  validation {
+    condition     = tonumber(split("/", var.vpc_cidr)[1]) >= 16 && tonumber(split("/", var.vpc_cidr)[1]) <= 24
+    error_message = "The VPC CIDR prefix must be between /16 and /24."
+  }
 }
 
 variable "environment" {
   description = "Environment name (e.g., dev, staging, prod)"
   type        = string
+
+  validation {
+    condition     = contains(["dev", "staging", "prod"], var.environment)
+    error_message = "Environment must be one of: dev, staging, prod."
+  }
 }
 
 variable "project" {
   description = "Project name for resource naming and tagging"
   type        = string
+
+  validation {
+    condition     = length(var.project) >= 2 && length(var.project) <= 32
+    error_message = "Project name must be between 2 and 32 characters."
+  }
 }
 
 variable "availability_zones" {
   description = "List of availability zones to use for subnets"
   type        = list(string)
+
+  validation {
+    condition     = length(var.availability_zones) >= 1 && length(var.availability_zones) <= 3
+    error_message = "You must specify between 1 and 3 availability zones."
+  }
 }
 
 variable "public_subnet_cidrs" {
