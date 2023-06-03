35bb8b824fda955a9560f884644793710a1c2694
kaustuvprajapati <kaustuvprajapati@gmail.com>
2023-06-03 10:40:00 +0100

    feat: add GitHub OIDC identity provider
    Create the GitHub Actions OIDC provider resource with TLS certificate
    thumbprint lookup. Wires up outputs for provider ARN and URL.
    Adds tls provider dependency for certificate data source.

--- modules/aws/iam/main.tf
+++ modules/aws/iam/main.tf
@@ -7,6 +7,35 @@
 #   - GitHub OIDC identity provider
 #   - CI plan role (read-only)
 #   - CI apply role (read-write)
-#
-# This module will be built incrementally across commits 21–24.
 # -----------------------------------------------------------------------------
+
+locals {
+  github_oidc_url = "https://token.actions.githubusercontent.com"
+  common_tags = merge(
+    {
+      Module      = "iam"
+      Project     = var.project
+      Environment = var.environment
+      ManagedBy   = "terraform"
+    },
+    var.tags,
+  )
+}
+
+# -----------------------------------------------------------------------------
+# GitHub OIDC Identity Provider
+# -----------------------------------------------------------------------------
+
+data "tls_certificate" "github" {
+  url = local.github_oidc_url
+}
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = local.github_oidc_url
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.github.certificates[0].sha1_fingerprint]
+
+  tags = merge(local.common_tags, {
+    Name = "${var.project}-github-oidc"
+  })
+}
--- modules/aws/iam/outputs.tf
+++ modules/aws/iam/outputs.tf
@@ -1,11 +1,11 @@
 output "oidc_provider_arn" {
   description = "ARN of the GitHub OIDC identity provider"
-  value       = ""
+  value       = aws_iam_openid_connect_provider.github.arn
 }
 
 output "oidc_provider_url" {
   description = "URL of the GitHub OIDC identity provider"
-  value       = ""
+  value       = aws_iam_openid_connect_provider.github.url
 }
 
 output "plan_role_arn" {
