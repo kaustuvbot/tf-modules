38baf9072da2be18d5c24ae6918ddf699b26b72e
kaustuvprajapati <kaustuvprajapati@gmail.com>
2023-06-03 12:05:00 +0100

    feat: add CI roles with plan/apply separation
    Create two OIDC-federated IAM roles for GitHub Actions:
    - ci-plan: ReadOnlyAccess for terraform plan on PRs
    - ci-apply: PowerUserAccess for terraform apply on merge
    
    Both roles use sub claim filtering to restrict access to
    configured repositories only.

--- modules/aws/iam/main.tf
+++ modules/aws/iam/main.tf
@@ -11,6 +11,12 @@
 
 locals {
   github_oidc_url = "https://token.actions.githubusercontent.com"
+
+  # Build sub claims for each repo: "repo:org/repo:*"
+  oidc_sub_claims = [
+    for repo in var.github_repositories : "repo:${repo}:*"
+  ]
+
   common_tags = merge(
     {
       Module      = "iam"
@@ -38,4 +44,90 @@ resource "aws_iam_openid_connect_provider" "github" {
   tags = merge(local.common_tags, {
     Name = "${var.project}-github-oidc"
   })
+}
+
+# -----------------------------------------------------------------------------
+# CI Plan Role (read-only)
+# -----------------------------------------------------------------------------
+
+data "aws_iam_policy_document" "plan_assume_role" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = local.oidc_sub_claims
+    }
+  }
+}
+
+resource "aws_iam_role" "plan" {
+  name               = "${var.project}-${var.environment}-ci-plan"
+  assume_role_policy = data.aws_iam_policy_document.plan_assume_role.json
+
+  tags = merge(local.common_tags, {
+    Name = "${var.project}-${var.environment}-ci-plan"
+    Role = "ci-plan"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "plan_read_only" {
+  role       = aws_iam_role.plan.name
+  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+}
+
+# -----------------------------------------------------------------------------
+# CI Apply Role (read-write)
+# -----------------------------------------------------------------------------
+
+data "aws_iam_policy_document" "apply_assume_role" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = local.oidc_sub_claims
+    }
+  }
+}
+
+resource "aws_iam_role" "apply" {
+  name               = "${var.project}-${var.environment}-ci-apply"
+  assume_role_policy = data.aws_iam_policy_document.apply_assume_role.json
+
+  tags = merge(local.common_tags, {
+    Name = "${var.project}-${var.environment}-ci-apply"
+    Role = "ci-apply"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "apply_power_user" {
+  role       = aws_iam_role.apply.name
+  policy_arn = "arn:aws:iam::aws:policy/PowerUserAccess"
 }
--- modules/aws/iam/outputs.tf
+++ modules/aws/iam/outputs.tf
@@ -10,10 +10,20 @@ output "oidc_provider_url" {
 
 output "plan_role_arn" {
   description = "ARN of the CI plan (read-only) role"
-  value       = ""
+  value       = aws_iam_role.plan.arn
+}
+
+output "plan_role_name" {
+  description = "Name of the CI plan role"
+  value       = aws_iam_role.plan.name
 }
 
 output "apply_role_arn" {
   description = "ARN of the CI apply (read-write) role"
-  value       = ""
+  value       = aws_iam_role.apply.arn
+}
+
+output "apply_role_name" {
+  description = "Name of the CI apply role"
+  value       = aws_iam_role.apply.name
 }
