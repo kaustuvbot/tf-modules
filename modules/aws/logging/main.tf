bb25e61a8f4ef81ecde656588ecb53ac9b2ee0c5
kaustuvprajapati <kaustuvprajapati@gmail.com>
2023-06-17 11:15:00 +0100

    feat: add CloudTrail organization trail
    Add central logging infrastructure: CloudWatch log group, S3 log
    bucket with encryption/versioning/lifecycle, and optional
    multi-region CloudTrail trail with CloudWatch integration.
    S3 bucket blocks public access and transitions logs to Glacier.

--- modules/aws/logging/main.tf
+++ modules/aws/logging/main.tf
@@ -10,7 +10,14 @@
 #   - AWS Config recorder (optional)
 # -----------------------------------------------------------------------------
 
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
 locals {
+  account_id = data.aws_caller_identity.current.account_id
+  region     = data.aws_region.current.name
+  bucket_name = "${var.project}-${var.environment}-logs-${local.account_id}"
+
   common_tags = merge(
     {
       Module      = "logging"
@@ -20,4 +27,183 @@ locals {
     },
     var.tags,
   )
+}
+
+# -----------------------------------------------------------------------------
+# Central CloudWatch Log Group
+# -----------------------------------------------------------------------------
+
+resource "aws_cloudwatch_log_group" "central" {
+  name              = "/${var.project}/${var.environment}/central"
+  retention_in_days = var.retention_in_days
+  kms_key_id        = var.kms_key_arn
+
+  tags = merge(local.common_tags, {
+    Name = "${var.project}-${var.environment}-central-logs"
+  })
+}
+
+# -----------------------------------------------------------------------------
+# S3 Bucket for Log Delivery
+# -----------------------------------------------------------------------------
+
+resource "aws_s3_bucket" "logs" {
+  bucket = local.bucket_name
+
+  tags = merge(local.common_tags, {
+    Name = local.bucket_name
+  })
+}
+
+resource "aws_s3_bucket_versioning" "logs" {
+  bucket = aws_s3_bucket.logs.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "logs" {
+  bucket = aws_s3_bucket.logs.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = var.kms_key_arn != null ? "aws:kms" : "AES256"
+      kms_master_key_id = var.kms_key_arn
+    }
+    bucket_key_enabled = var.kms_key_arn != null
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "logs" {
+  bucket = aws_s3_bucket.logs.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "logs" {
+  bucket = aws_s3_bucket.logs.id
+
+  rule {
+    id     = "expire-old-logs"
+    status = "Enabled"
+
+    expiration {
+      days = var.retention_in_days
+    }
+
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+
+    transition {
+      days          = 60
+      storage_class = "GLACIER"
+    }
+  }
+}
+
+# -----------------------------------------------------------------------------
+# CloudTrail
+# -----------------------------------------------------------------------------
+
+resource "aws_s3_bucket_policy" "cloudtrail" {
+  count  = var.enable_cloudtrail ? 1 : 0
+  bucket = aws_s3_bucket.logs.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "AWSCloudTrailAclCheck"
+        Effect    = "Allow"
+        Principal = { Service = "cloudtrail.amazonaws.com" }
+        Action    = "s3:GetBucketAcl"
+        Resource  = aws_s3_bucket.logs.arn
+      },
+      {
+        Sid       = "AWSCloudTrailWrite"
+        Effect    = "Allow"
+        Principal = { Service = "cloudtrail.amazonaws.com" }
+        Action    = "s3:PutObject"
+        Resource  = "${aws_s3_bucket.logs.arn}/cloudtrail/AWSLogs/${local.account_id}/*"
+        Condition = {
+          StringEquals = { "s3:x-amz-acl" = "bucket-owner-full-control" }
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_cloudwatch_log_group" "cloudtrail" {
+  count             = var.enable_cloudtrail ? 1 : 0
+  name              = "/${var.project}/${var.environment}/cloudtrail"
+  retention_in_days = var.retention_in_days
+  kms_key_id        = var.kms_key_arn
+
+  tags = merge(local.common_tags, {
+    Name = "${var.project}-${var.environment}-cloudtrail-logs"
+  })
+}
+
+data "aws_iam_policy_document" "cloudtrail_cloudwatch" {
+  count = var.enable_cloudtrail ? 1 : 0
+
+  statement {
+    effect  = "Allow"
+    actions = ["logs:CreateLogStream", "logs:PutLogEvents"]
+    resources = [
+      "${aws_cloudwatch_log_group.cloudtrail[0].arn}:*"
+    ]
+  }
+}
+
+resource "aws_iam_role" "cloudtrail_cloudwatch" {
+  count = var.enable_cloudtrail ? 1 : 0
+  name  = "${var.project}-${var.environment}-cloudtrail-cw"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = { Service = "cloudtrail.amazonaws.com" }
+        Action    = "sts:AssumeRole"
+      },
+    ]
+  })
+
+  tags = local.common_tags
+}
+
+resource "aws_iam_role_policy" "cloudtrail_cloudwatch" {
+  count  = var.enable_cloudtrail ? 1 : 0
+  name   = "cloudtrail-cloudwatch-logs"
+  role   = aws_iam_role.cloudtrail_cloudwatch[0].id
+  policy = data.aws_iam_policy_document.cloudtrail_cloudwatch[0].json
+}
+
+resource "aws_cloudtrail" "this" {
+  count = var.enable_cloudtrail ? 1 : 0
+
+  name                          = "${var.project}-${var.environment}-trail"
+  s3_bucket_name                = aws_s3_bucket.logs.id
+  s3_key_prefix                 = "cloudtrail"
+  include_global_service_events = true
+  is_multi_region_trail         = true
+  enable_log_file_validation    = true
+  kms_key_id                    = var.kms_key_arn
+
+  cloud_watch_logs_group_arn = "${aws_cloudwatch_log_group.cloudtrail[0].arn}:*"
+  cloud_watch_logs_role_arn  = aws_iam_role.cloudtrail_cloudwatch[0].arn
+
+  tags = merge(local.common_tags, {
+    Name = "${var.project}-${var.environment}-trail"
+  })
+
+  depends_on = [aws_s3_bucket_policy.cloudtrail]
 }
--- modules/aws/logging/outputs.tf
+++ modules/aws/logging/outputs.tf
@@ -1,26 +1,26 @@
 output "log_group_name" {
   description = "Name of the central CloudWatch log group"
-  value       = ""
+  value       = aws_cloudwatch_log_group.central.name
 }
 
 output "log_group_arn" {
   description = "ARN of the central CloudWatch log group"
-  value       = ""
+  value       = aws_cloudwatch_log_group.central.arn
 }
 
 output "log_bucket_id" {
   description = "ID of the S3 bucket for log delivery"
-  value       = ""
+  value       = aws_s3_bucket.logs.id
 }
 
 output "log_bucket_arn" {
   description = "ARN of the S3 bucket for log delivery"
-  value       = ""
+  value       = aws_s3_bucket.logs.arn
 }
 
 output "cloudtrail_arn" {
   description = "ARN of the CloudTrail trail (if enabled)"
-  value       = ""
+  value       = var.enable_cloudtrail ? aws_cloudtrail.this[0].arn : null
 }
 
 output "config_recorder_id" {
