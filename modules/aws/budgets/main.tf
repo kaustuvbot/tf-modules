02f85569609d79a766877609dac47fee95e0fd13
kaustuvprajapati <kaustuvprajapati@gmail.com>
2023-07-15 11:00:00 +0100

    feat: add monthly and forecast budget alerts
    Create two AWS Budgets resources per environment: a monthly actual
    spend budget (alerts at 80%, 100%, 110% forecast) and a separate
    forecast budget for early warning at 90%. Supports tag-based cost
    filters via dynamic block. Email subscribers configurable.

--- modules/aws/budgets/main.tf
+++ modules/aws/budgets/main.tf
@@ -4,9 +4,98 @@
 # Manages cost budgets and anomaly detection for AWS spend governance.
 #
 # Resources created:
-#   - Monthly cost budget with threshold alerts
-#   - Forecasted spend budget
+#   - Monthly cost budget with threshold alerts (80% actual, 100% actual, 110% forecast)
 #   - Cost Anomaly Detection monitor and subscription
-#
-# This module will be built incrementally across commits 51–53.
 # -----------------------------------------------------------------------------
+
+locals {
+  budget_name_prefix = "${var.project}-${var.environment}"
+
+  # Build notification subscriber list from email addresses
+  subscribers = [
+    for email in var.alert_email_addresses : {
+      subscription_type = "EMAIL"
+      address           = email
+    }
+  ]
+}
+
+# -----------------------------------------------------------------------------
+# Monthly Cost Budget
+# -----------------------------------------------------------------------------
+
+resource "aws_budgets_budget" "monthly" {
+  name         = "${local.budget_name_prefix}-monthly"
+  budget_type  = "COST"
+  limit_amount = tostring(var.monthly_budget_amount)
+  limit_unit   = var.currency
+  time_unit    = "MONTHLY"
+
+  dynamic "cost_filter" {
+    for_each = var.cost_filters
+
+    content {
+      name   = cost_filter.key
+      values = [cost_filter.value]
+    }
+  }
+
+  # Alert at 80% actual spend
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 80
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "ACTUAL"
+    subscriber_email_addresses = var.alert_email_addresses
+  }
+
+  # Alert at 100% actual spend
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 100
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "ACTUAL"
+    subscriber_email_addresses = var.alert_email_addresses
+  }
+
+  # Alert when forecast exceeds 110% of budget
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 110
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "FORECASTED"
+    subscriber_email_addresses = var.alert_email_addresses
+  }
+}
+
+# -----------------------------------------------------------------------------
+# Forecasted Spend Budget
+# -----------------------------------------------------------------------------
+# Separate budget that triggers earlier — warns when forecast will exceed
+# budget before the month ends.
+
+resource "aws_budgets_budget" "forecast" {
+  name         = "${local.budget_name_prefix}-forecast"
+  budget_type  = "COST"
+  limit_amount = tostring(var.monthly_budget_amount)
+  limit_unit   = var.currency
+  time_unit    = "MONTHLY"
+
+  dynamic "cost_filter" {
+    for_each = var.cost_filters
+
+    content {
+      name   = cost_filter.key
+      values = [cost_filter.value]
+    }
+  }
+
+  # Early warning: forecast exceeds 90% of budget
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 90
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "FORECASTED"
+    subscriber_email_addresses = var.alert_email_addresses
+  }
+}
--- modules/aws/budgets/outputs.tf
+++ modules/aws/budgets/outputs.tf
@@ -1,11 +1,11 @@
 output "monthly_budget_name" {
   description = "Name of the monthly cost budget"
-  value       = ""
+  value       = aws_budgets_budget.monthly.name
 }
 
 output "forecast_budget_name" {
   description = "Name of the forecast budget"
-  value       = ""
+  value       = aws_budgets_budget.forecast.name
 }
 
 output "anomaly_monitor_arn" {
