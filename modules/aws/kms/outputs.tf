7cdd424f30c2b46c8d449d320efbf18a3ad5d19e
kaustuvprajapati <kaustuvprajapati@gmail.com>
2023-08-07 14:00:00 +0100

    feat: add KMS alias outputs to simplify cross-module referencing

--- modules/aws/kms/outputs.tf
+++ modules/aws/kms/outputs.tf
@@ -27,3 +27,18 @@ output "general_key_id" {
   description = "ID of the general-purpose KMS key"
   value       = var.enable_general_key ? aws_kms_key.general[0].key_id : null
 }
+
+output "logs_key_alias" {
+  description = "Alias name for the logs KMS key"
+  value       = var.enable_logs_key ? aws_kms_alias.logs[0].name : null
+}
+
+output "state_key_alias" {
+  description = "Alias name for the state KMS key"
+  value       = var.enable_state_key ? aws_kms_alias.state[0].name : null
+}
+
+output "general_key_alias" {
+  description = "Alias name for the general-purpose KMS key"
+  value       = var.enable_general_key ? aws_kms_alias.general[0].name : null
+}
