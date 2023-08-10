fdf2e1cb5c5a1543c62be494bf88ef4f99abfd16
kaustuvprajapati <kaustuvprajapati@gmail.com>
2023-08-10 14:00:00 +0100

    feat: rewrite prod environment with KMS, flow logs, and hardened EKS

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,29 +1,132 @@
 terraform {
   required_version = ">= 1.4.0, < 2.0.0"
 
-  # backend "s3" {
-  #   bucket         = "<project>-terraform-state-<region>"
-  #   key            = "prod/platform/terraform.tfstate"
-  #   region         = "<region>"
-  #   dynamodb_table = "<project>-terraform-lock"
-  #   encrypt        = true
-  # }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0.0, < 6.0.0"
+    }
+  }
+
+  backend "s3" {
+    # bucket         = "<project>-prod-tfstate-<account-id>"
+    # key            = "prod/platform/terraform.tfstate"
+    # region         = "us-east-1"
+    # dynamodb_table = "<project>-prod-tflock"
+    # encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = var.region
+
+  default_tags {
+    tags = {
+      Project     = var.project
+      Environment = "prod"
+      ManagedBy   = "terraform"
+    }
+  }
 }
 
 locals {
   environment = "prod"
+}
+
+# -----------------------------------------------------------------------------
+# Encryption
+# -----------------------------------------------------------------------------
+
+module "kms" {
+  source = "../../modules/aws/kms"
+
   project     = var.project
+  environment = local.environment
+
+  enable_logs_key    = true
+  enable_state_key   = true
+  enable_general_key = true
 }
 
-module "naming" {
-  source      = "../../modules/core/naming"
-  project     = local.project
+# -----------------------------------------------------------------------------
+# Networking
+# -----------------------------------------------------------------------------
+
+module "vpc" {
+  source = "../../modules/aws/vpc"
+
+  project     = var.project
   environment = local.environment
+
+  vpc_cidr             = var.vpc_cidr
+  availability_zones   = var.availability_zones
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+  enable_nat_gateway   = true
+  single_nat_gateway   = false  # per-AZ NAT in prod for HA
+
+  enable_flow_logs                    = true
+  flow_logs_destination               = "cloud-watch-logs"
+  flow_logs_cloudwatch_log_group_name = "/aws/${var.project}/prod/vpc-flow-logs"
 }
 
-module "tagging" {
-  source      = "../../modules/core/tagging"
-  project     = local.project
+# -----------------------------------------------------------------------------
+# Kubernetes
+# -----------------------------------------------------------------------------
+
+module "eks" {
+  source = "../../modules/aws/eks"
+
+  project     = var.project
   environment = local.environment
-  extra_tags  = var.extra_tags
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnet_ids
+
+  cluster_version        = var.eks_cluster_version
+  kms_key_arn            = module.kms.general_key_arn
+  endpoint_public_access = false  # private cluster in prod
+  imdsv2_required        = true
+
+  node_groups = {
+    system = {
+      instance_types = ["m5.large"]
+      min_size       = 2
+      max_size       = 6
+      desired_size   = 2
+    }
+  }
 }
+
+module "eks_addons" {
+  source = "../../modules/aws/eks-addons"
+
+  project     = var.project
+  environment = local.environment
+
+  cluster_name           = module.eks.cluster_name
+  cluster_endpoint       = module.eks.cluster_endpoint
+  cluster_ca_certificate = module.eks.cluster_certificate_authority_data
+  region                 = var.region
+  oidc_provider_arn      = module.eks.oidc_provider_arn
+  oidc_provider_url      = module.eks.oidc_provider_url
+  vpc_id                 = module.vpc.vpc_id
+
+  enable_alb_controller = true
+  enable_cert_manager   = true
+}
+
+# -----------------------------------------------------------------------------
+# Monitoring
+# -----------------------------------------------------------------------------
+
+module "monitoring" {
+  source = "../../modules/aws/monitoring"
+
+  project     = var.project
+  environment = local.environment
+
+  cluster_name          = module.eks.cluster_name
+  alarm_email_addresses = var.alarm_email_addresses
+}
+
--- environments/prod/variables.tf
+++ environments/prod/variables.tf
@@ -3,8 +3,44 @@ variable "project" {
   type        = string
 }
 
-variable "extra_tags" {
-  description = "Additional tags for this environment"
-  type        = map(string)
-  default     = {}
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+  default     = "10.1.0.0/16"
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.1.10.0/24", "10.1.11.0/24", "10.1.12.0/24"]
+}
+
+variable "eks_cluster_version" {
+  description = "Kubernetes version for the EKS cluster"
+  type        = string
+  default     = "1.28"
+}
+
+variable "alarm_email_addresses" {
+  description = "Email addresses for CloudWatch alarm notifications"
+  type        = list(string)
+  default     = []
 }
