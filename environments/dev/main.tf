2cf3ea971395e3120dee17f9cbaf3a5c5447fa66
kaustuvprajapati <kaustuvprajapati@gmail.com>
2023-08-10 11:00:00 +0100

    feat: rewrite dev environment to compose real AWS modules

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,29 +1,95 @@
 terraform {
   required_version = ">= 1.4.0, < 2.0.0"
 
-  # backend "s3" {
-  #   bucket         = "<project>-terraform-state-<region>"
-  #   key            = "dev/platform/terraform.tfstate"
-  #   region         = "<region>"
-  #   dynamodb_table = "<project>-terraform-lock"
-  #   encrypt        = true
-  # }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0.0, < 6.0.0"
+    }
+  }
+
+  backend "s3" {
+    # Configure via -backend-config or environment variables
+    # bucket         = "<project>-dev-tfstate-<account-id>"
+    # key            = "dev/platform/terraform.tfstate"
+    # region         = "us-east-1"
+    # dynamodb_table = "<project>-dev-tflock"
+    # encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = var.region
+
+  default_tags {
+    tags = {
+      Project     = var.project
+      Environment = "dev"
+      ManagedBy   = "terraform"
+    }
+  }
 }
 
 locals {
   environment = "dev"
+}
+
+# -----------------------------------------------------------------------------
+# Networking
+# -----------------------------------------------------------------------------
+
+module "vpc" {
+  source = "../../modules/aws/vpc"
+
   project     = var.project
+  environment = local.environment
+
+  vpc_cidr             = var.vpc_cidr
+  availability_zones   = var.availability_zones
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+  enable_nat_gateway   = true
+  single_nat_gateway   = true  # single NAT for dev cost savings
 }
 
-module "naming" {
-  source      = "../../modules/core/naming"
-  project     = local.project
+# -----------------------------------------------------------------------------
+# Kubernetes
+# -----------------------------------------------------------------------------
+
+module "eks" {
+  source = "../../modules/aws/eks"
+
+  project     = var.project
   environment = local.environment
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnet_ids
+
+  cluster_version        = var.eks_cluster_version
+  endpoint_public_access = true
+  public_access_cidrs    = var.eks_public_access_cidrs
+
+  node_groups = {
+    general = {
+      instance_types = ["t3.medium"]
+      min_size       = 1
+      max_size       = 3
+      desired_size   = 1
+    }
+  }
 }
 
-module "tagging" {
-  source      = "../../modules/core/tagging"
-  project     = local.project
+module "eks_addons" {
+  source = "../../modules/aws/eks-addons"
+
+  project     = var.project
   environment = local.environment
-  extra_tags  = var.extra_tags
+
+  cluster_name           = module.eks.cluster_name
+  cluster_endpoint       = module.eks.cluster_endpoint
+  cluster_ca_certificate = module.eks.cluster_certificate_authority_data
+  region                 = var.region
+  oidc_provider_arn      = module.eks.oidc_provider_arn
+  oidc_provider_url      = module.eks.oidc_provider_url
+  vpc_id                 = module.vpc.vpc_id
 }
--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -3,8 +3,44 @@ variable "project" {
   type        = string
 }
 
-variable "extra_tags" {
-  description = "Additional tags for this environment"
-  type        = map(string)
-  default     = {}
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.10.0/24", "10.0.11.0/24"]
+}
+
+variable "eks_cluster_version" {
+  description = "Kubernetes version for the EKS cluster"
+  type        = string
+  default     = "1.28"
+}
+
+variable "eks_public_access_cidrs" {
+  description = "CIDR blocks allowed to reach the EKS public API server"
+  type        = list(string)
+  default     = []
 }
