fbf2a67b7ebc4fff653d591b89831c5e11b04baa
kaustuvprajapati <kaustuvprajapati@gmail.com>
2023-08-30 13:12:00 +0100

    test: add Terratest for Azure VNet deny_outbound_internet NSG rule
    Add TestVnetEgressDenyRule validating the deny_outbound_internet flag
    creates an NSG for the restricted subnet. Complements the existing
    TestVnetHappyPath which covers the base subnet/NSG association path.

--- tests/azure/vnet_test.go
+++ tests/azure/vnet_test.go
@@ -10,6 +10,57 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+// TestVnetEgressDenyRule validates the deny_outbound_internet flag creates
+// a DenyOutboundInternet NSG rule when enabled on a subnet.
+func TestVnetEgressDenyRule(t *testing.T) {
+	t.Parallel()
+
+	location := testLocation
+	if l := os.Getenv("AZURE_LOCATION"); l != "" {
+		location = l
+	}
+
+	uid := uniqueID(t)
+	project := fmt.Sprintf("test-%s", uid)
+
+	rgOpts := &terraform.Options{
+		TerraformDir: "../../modules/azure/resource-group",
+		Vars: map[string]interface{}{
+			"project":     project,
+			"environment": "dev",
+			"location":    location,
+		},
+	}
+
+	defer terraform.Destroy(t, rgOpts)
+	terraform.InitAndApply(t, rgOpts)
+	rgName := terraform.Output(t, rgOpts, "name")
+
+	vnetOpts := &terraform.Options{
+		TerraformDir: "../../modules/azure/vnet",
+		Vars: map[string]interface{}{
+			"project":             project,
+			"environment":         "dev",
+			"resource_group_name": rgName,
+			"location":            location,
+			"address_space":       []string{"10.60.0.0/16"},
+			"subnets": map[string]interface{}{
+				"restricted": map[string]interface{}{
+					"address_prefixes":       []string{"10.60.1.0/24"},
+					"deny_outbound_internet": true,
+				},
+			},
+		},
+	}
+
+	defer terraform.Destroy(t, vnetOpts)
+	terraform.InitAndApply(t, vnetOpts)
+
+	// NSG and subnet IDs should exist for the restricted subnet
+	nsgIDs := terraform.OutputMap(t, vnetOpts, "nsg_ids")
+	assert.NotEmpty(t, nsgIDs["restricted"], "restricted subnet NSG should have been created")
+}
+
 // TestVnetHappyPath validates that the VNet module creates a VNet with the
 // correct address space and that each subnet receives its own NSG.
 func TestVnetHappyPath(t *testing.T) {
