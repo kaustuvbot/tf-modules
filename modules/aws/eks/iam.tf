f3ae0ab866175d7b3c8a2ed8af0b8e0596743838
kaustuvprajapati <kaustuvprajapati@gmail.com>
2023-09-23 19:54:24 +0100

    refactor: extract IAM roles and policies from EKS main.tf into dedicated iam.tf
    Move cluster IAM role, node group IAM role, five policy attachments, and
    cluster autoscaler IRSA resources into iam.tf. main.tf now contains only the
    CloudWatch log group, EKS cluster, managed node groups, and OIDC provider —
    reducing it from 305 to ~120 lines. Improves navigability with no behavior
    change; all resource addresses are unchanged.

---
+++ modules/aws/eks/iam.tf
@@ -0,0 +1,164 @@
+# -----------------------------------------------------------------------------
+# EKS IAM Roles and Policy Attachments
+# -----------------------------------------------------------------------------
+# Cluster role: assumed by the EKS control plane.
+# Node role: assumed by EC2 worker nodes via instance profile.
+# Autoscaler IRSA role: optional OIDC-scoped role for Cluster Autoscaler.
+# -----------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
+# Cluster IAM Role
+# -----------------------------------------------------------------------------
+
+resource "aws_iam_role" "cluster" {
+  name = "${local.cluster_name}-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = { Service = "eks.amazonaws.com" }
+        Action    = "sts:AssumeRole"
+      },
+    ]
+  })
+
+  tags = merge(local.common_tags, {
+    Name = "${local.cluster_name}-cluster-role"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_policy" {
+  role       = aws_iam_role.cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_vpc_controller" {
+  role       = aws_iam_role.cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+}
+
+# -----------------------------------------------------------------------------
+# Node Group IAM Role
+# -----------------------------------------------------------------------------
+
+resource "aws_iam_role" "node_group" {
+  name = "${local.cluster_name}-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = { Service = "ec2.amazonaws.com" }
+        Action    = "sts:AssumeRole"
+      },
+    ]
+  })
+
+  tags = merge(local.common_tags, {
+    Name = "${local.cluster_name}-node-role"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "node_worker" {
+  role       = aws_iam_role.node_group.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "node_cni" {
+  role       = aws_iam_role.node_group.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "node_ecr" {
+  role       = aws_iam_role.node_group.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+# -----------------------------------------------------------------------------
+# Cluster Autoscaler IRSA Role (optional)
+# -----------------------------------------------------------------------------
+
+data "aws_iam_policy_document" "cluster_autoscaler_assume" {
+  count = var.enable_cluster_autoscaler_irsa ? 1 : 0
+
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.eks.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:cluster-autoscaler"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "cluster_autoscaler" {
+  count = var.enable_cluster_autoscaler_irsa ? 1 : 0
+
+  name               = "${local.cluster_name}-cluster-autoscaler"
+  assume_role_policy = data.aws_iam_policy_document.cluster_autoscaler_assume[0].json
+
+  tags = merge(local.common_tags, {
+    Name    = "${local.cluster_name}-cluster-autoscaler"
+    AddOn   = "cluster-autoscaler"
+    k8s-app = "cluster-autoscaler"
+  })
+}
+
+resource "aws_iam_role_policy" "cluster_autoscaler" {
+  count = var.enable_cluster_autoscaler_irsa ? 1 : 0
+
+  name = "cluster-autoscaler"
+  role = aws_iam_role.cluster_autoscaler[0].id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "autoscaling:DescribeAutoScalingGroups",
+          "autoscaling:DescribeAutoScalingInstances",
+          "autoscaling:DescribeLaunchConfigurations",
+          "autoscaling:DescribeScalingActivities",
+          "autoscaling:DescribeTags",
+          "ec2:DescribeImages",
+          "ec2:DescribeInstanceTypes",
+          "ec2:DescribeLaunchTemplateVersions",
+          "ec2:GetInstanceTypesFromInstanceRequirements",
+          "eks:DescribeNodegroup",
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "autoscaling:SetDesiredCapacity",
+          "autoscaling:TerminateInstanceInAutoScalingGroup",
+        ]
+        Resource = "*"
+        Condition = {
+          StringEquals = {
+            "autoscaling:ResourceTag/k8s.io/cluster-autoscaler/enabled"               = "true"
+            "autoscaling:ResourceTag/k8s.io/cluster-autoscaler/${local.cluster_name}" = "owned"
+          }
+        }
+      },
+    ]
+  })
+}
--- modules/aws/eks/main.tf
+++ modules/aws/eks/main.tf
@@ -4,9 +4,12 @@
 # Manages an EKS cluster with managed node groups.
 #
 # Resources created:
-#   - EKS cluster with IAM role
-#   - Managed node groups with IAM role
-#   - OIDC provider for IRSA (added in commit 38)
+#   - EKS cluster (this file)
+#   - Managed node groups (this file)
+#   - OIDC provider for IRSA (this file)
+#   - IAM roles and policy attachments (iam.tf)
+#   - Node group launch template (launch-template.tf)
+#   - Managed add-ons (managed-addons.tf)
 # -----------------------------------------------------------------------------
 
 # -----------------------------------------------------------------------------
@@ -23,39 +26,6 @@ resource "aws_cloudwatch_log_group" "eks_cluster" {
   tags = merge(local.common_tags, {
     Name = "/aws/eks/${local.cluster_name}/cluster"
   })
-}
-
-# -----------------------------------------------------------------------------
-# Cluster IAM Role
-# -----------------------------------------------------------------------------
-
-resource "aws_iam_role" "cluster" {
-  name = "${local.cluster_name}-cluster-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect    = "Allow"
-        Principal = { Service = "eks.amazonaws.com" }
-        Action    = "sts:AssumeRole"
-      },
-    ]
-  })
-
-  tags = merge(local.common_tags, {
-    Name = "${local.cluster_name}-cluster-role"
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "cluster_policy" {
-  role       = aws_iam_role.cluster.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "cluster_vpc_controller" {
-  role       = aws_iam_role.cluster.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
 }
 
 # -----------------------------------------------------------------------------
@@ -101,44 +71,6 @@ resource "aws_eks_cluster" "this" {
     aws_iam_role_policy_attachment.cluster_vpc_controller,
     aws_cloudwatch_log_group.eks_cluster,
   ]
-}
-
-# -----------------------------------------------------------------------------
-# Node Group IAM Role
-# -----------------------------------------------------------------------------
-
-resource "aws_iam_role" "node_group" {
-  name = "${local.cluster_name}-node-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect    = "Allow"
-        Principal = { Service = "ec2.amazonaws.com" }
-        Action    = "sts:AssumeRole"
-      },
-    ]
-  })
-
-  tags = merge(local.common_tags, {
-    Name = "${local.cluster_name}-node-role"
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "node_worker" {
-  role       = aws_iam_role.node_group.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "node_cni" {
-  role       = aws_iam_role.node_group.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-}
-
-resource "aws_iam_role_policy_attachment" "node_ecr" {
-  role       = aws_iam_role.node_group.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
 # -----------------------------------------------------------------------------
@@ -218,88 +150,3 @@ resource "aws_iam_openid_connect_provider" "eks" {
   })
 }
 
-# -----------------------------------------------------------------------------
-# Cluster Autoscaler IRSA Role (optional)
-# -----------------------------------------------------------------------------
-
-data "aws_iam_policy_document" "cluster_autoscaler_assume" {
-  count = var.enable_cluster_autoscaler_irsa ? 1 : 0
-
-  statement {
-    effect  = "Allow"
-    actions = ["sts:AssumeRoleWithWebIdentity"]
-
-    principals {
-      type        = "Federated"
-      identifiers = [aws_iam_openid_connect_provider.eks.arn]
-    }
-
-    condition {
-      test     = "StringEquals"
-      variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub"
-      values   = ["system:serviceaccount:kube-system:cluster-autoscaler"]
-    }
-
-    condition {
-      test     = "StringEquals"
-      variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:aud"
-      values   = ["sts.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role" "cluster_autoscaler" {
-  count = var.enable_cluster_autoscaler_irsa ? 1 : 0
-
-  name               = "${local.cluster_name}-cluster-autoscaler"
-  assume_role_policy = data.aws_iam_policy_document.cluster_autoscaler_assume[0].json
-
-  tags = merge(local.common_tags, {
-    Name    = "${local.cluster_name}-cluster-autoscaler"
-    AddOn   = "cluster-autoscaler"
-    k8s-app = "cluster-autoscaler"
-  })
-}
-
-resource "aws_iam_role_policy" "cluster_autoscaler" {
-  count = var.enable_cluster_autoscaler_irsa ? 1 : 0
-
-  name = "cluster-autoscaler"
-  role = aws_iam_role.cluster_autoscaler[0].id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "autoscaling:DescribeAutoScalingGroups",
-          "autoscaling:DescribeAutoScalingInstances",
-          "autoscaling:DescribeLaunchConfigurations",
-          "autoscaling:DescribeScalingActivities",
-          "autoscaling:DescribeTags",
-          "ec2:DescribeImages",
-          "ec2:DescribeInstanceTypes",
-          "ec2:DescribeLaunchTemplateVersions",
-          "ec2:GetInstanceTypesFromInstanceRequirements",
-          "eks:DescribeNodegroup",
-        ]
-        Resource = "*"
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "autoscaling:SetDesiredCapacity",
-          "autoscaling:TerminateInstanceInAutoScalingGroup",
-        ]
-        Resource = "*"
-        Condition = {
-          StringEquals = {
-            "autoscaling:ResourceTag/k8s.io/cluster-autoscaler/enabled"               = "true"
-            "autoscaling:ResourceTag/k8s.io/cluster-autoscaler/${local.cluster_name}" = "owned"
-          }
-        }
-      },
-    ]
-  })
-}
