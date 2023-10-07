be1f3b4d017122300a81da2b5b5c1cd0218873d1
kaustuvprajapati <kaustuvprajapati@gmail.com>
2023-10-07 20:19:17 +0100

    refactor: normalize required_providers version constraints across all modules
    Standardize Azure provider to ~> 3.0 (equivalent to >= 3.0, < 4.0) across
    all azure/* modules, replacing the inconsistent ">= 3.50.0, < 4.0.0" pin
    used in aks/key-vault/monitoring/resource-group/vnet. Normalize Helm provider
    to ~> 2.0 and Kubernetes provider to ~> 2.0 in eks-addons, removing the
    minor-version pins (~> 2.11, ~> 2.23) that forced unnecessary upgrades.
    Consistent constraint syntax reduces provider churn in consumer repositories.

--- modules/aws/eks-addons/versions.tf
+++ modules/aws/eks-addons/versions.tf
@@ -8,11 +8,11 @@ terraform {
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 2.11"
+      version = "~> 2.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.23"
+      version = "~> 2.0"
     }
   }
 }
